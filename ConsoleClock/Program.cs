920243c2cb48f47cccd62f806489808ed36c0f57
Sheshikanth Errabelly <Sheshikanth.vr510@gmail.com>
2019-05-21 14:17:12 -0400

    Add files via upload

---
+++ ConsoleClock/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleClock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program prog = new Program();
+            ThreadStart Ts = new ThreadStart(prog.showClock);
+            Thread T = new Thread(Ts);
+            T.Start();
+            Console.ReadLine();
+        }
+
+        public void showClock()
+        {
+            for(;;)
+            {
+                Console.WriteLine(DateTime.Now.ToString());
+                Console.WriteLine("\a");
+
+                Thread.Sleep(1000);
+
+                Console.Clear();
+            }
+        }
+
+
+    }
+}
